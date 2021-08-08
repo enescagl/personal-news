be97cab8c5cdce7f186545747408ca40f233cf9a
Enes Çağlıyan <enes.cagliyan@gmail.com>
2021-08-08 11:24:07 +0300

    added simple jwt for auth.
    Added also session auth temporarily for browsable api.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -76,6 +77,13 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
 
 # Password validation
