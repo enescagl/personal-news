3aa2c8597bbf8b0e60c415a338fd8f92b2829ffb
Enes Çağlıyan <enes.cagliyan@gmail.com>
2021-08-10 04:25:21 +0300

    creating dummy data fixes

--- backend/news/management/commands/createdummydata.py
+++ backend/news/management/commands/createdummydata.py
@@ -2,30 +2,41 @@ from django.core.management.base import BaseCommand, CommandError
 from news.models import News
 import random
 import requests
+import os
+from pathlib import Path
 
 
 class Command(BaseCommand):
     help = 'Adds 100 dummy news to database.'
 
-    # def add_arguments(self, parser):
-    # url = 'http://placeimg.com/640/480/arch'
-    # for index in range(100):
-    #     with open(f"static/img/cover_images/{index}.jpg", 'wb') as f:
-    #         r = requests.get(url, allow_redirects=True)
-    #         f.write(r.content)
-
     def handle(self, *args, **options):
+
+        dir_name = str(Path(__file__).parent.parent.parent.parent
+                       ) + '\\media\\img\\cover_images\\'
+        if os.path.exists(dir_name) and os.path.isdir(dir_name):
+            if not os.listdir(dir_name):
+                url = 'http://placeimg.com/640/480/arch'
+                for index in range(100):
+                    with open(f'media/img/cover_images/{index}.jpg',
+                              'wb') as f:
+                        r = requests.get(url, allow_redirects=True)
+                        f.write(r.content)
+            else:
+                print('Directory is not empty')
+        else:
+            print('Given directory doesn\'t exist')
+
         random_length = random.randint(3, 10)
 
         list_of_news = [
             News(
-                heading=f"{index} - News About Something",
+                heading=f'{index} - News About Something',
                 body=requests.get(
-                    f"https://loripsum.net/api/{random_length}/plaintext").
+                    f'https://loripsum.net/api/{random_length}/plaintext').
                 text,
                 short_description=requests.get(
-                    "https://loripsum.net/api/1/short/plaintext").text,
-                cover_image=f"static/img/cover_images/{index}.jpg",
+                    'https://loripsum.net/api/1/short/plaintext').text,
+                cover_image=f'img/cover_images/{index}.jpg',
             ) for index in range(100)
         ]
-        new_news = News.objects.bulk_create(list(list_of_news))
+        News.objects.bulk_create(list(list_of_news))
